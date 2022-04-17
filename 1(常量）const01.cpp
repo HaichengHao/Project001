eebd4fd802ad86f1fce4fa717ba4997d15f4e87e
hero <1326172435@qq.com>
2022-04-17 23:01:31 +0800

    chapter

---
+++ 1(常量）const01.cpp
@@ -0,0 +1,17 @@
+//#include<stdio.h>
+//void main() {
+//	int n1 = 0213;//八进制，对应十进制的139
+//	int n2 = 0x4b;//十六进制，对应十进制的75
+//	char c1 = 'a';
+//	char c2 = '\t';
+//
+//	char str1[20] = "北京 hello";
+//	char str2[100] = "hello \
+//world";//等价于"hello world"
+//	printf("n1 = %d,n2=%d", n1, n2);
+//	printf("\nstr1=%s str2=%s", str1, str2);
+//}
+///*
+//n1 = 139,n2=75
+//str1=北京 hello str2=hello world
+//*/
