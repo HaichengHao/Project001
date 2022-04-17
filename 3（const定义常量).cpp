7880bcb9dbc6ec3c3238c0eaa4eea06fb2691b2f
hero <1326172435@qq.com>
2022-04-17 23:01:46 +0800

    chapter

---
+++ 3（const定义常量).cpp
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+////const是一个关键字，规定好，表示后面定义了一个常量
+////PI是常量名，即是一个常量，常量的值为3.14
+////PI是常量，因此一旦定义那么在引用的时候便不能修改
+////const定义常量时，需要加分号
+//const double PI = 3.14;//用const关键字定义常量PI=3.14，注意，const关键字和#define的用法细节有所不同
+//int main(){
+//	double area;
+//	double r = 1.2;
+//	area = PI * r * r;
+//	printf("圆的面积为：%.2f", area);
+//	return 0;
+//}
+///*
+//圆的面积为：4.52
+//*/
