7404240edaf434a65defb0465dfc49ae914bae4b
hero <1326172435@qq.com>
2022-04-17 23:01:38 +0800

    chapter

---
+++ 2.(define定义常量).cpp
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//#define PI 3.14//定义PI 为 3.14
+////思考，可以修改PI 为 3.1415么？答案：不可修改，因为我们上一步已经定义过PI是一个常量
+//int main() {//这里不是void main ,因为我们这里需要输入
+//	double area;
+//	double r = 1.2;
+//	area = PI * r * r;
+//	printf("圆的面积为%.2f", area);
+//	return 0;//因为我们有输入，所以需要有返回值
+//}
+///*
+//圆的面积为4.52
+//*/
