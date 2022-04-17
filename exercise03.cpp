4f4f31f1124ca38bc51c0f85acb4a8cfe5e10dbb
hero <1326172435@qq.com>
2022-04-17 23:04:01 +0800

    new

---
+++ exercise03.cpp
@@ -0,0 +1,36 @@
+//#include<stdio.h>
+//void main(){
+//	//----小小计算机-----
+//	//1.定义两个int
+//	//2.根据要求进行计算，得到不同的结果，可以再定义变量
+//	int n1 = 10;
+//	int n2 = 5;
+//	int num = 10;
+//	int sum = n1 + n2;//加
+//	int sub = n1 - n2;//减
+//	int mul = n1 * n2;//乘
+//	int div = n1 / n2;//除
+//	int mod = n1 % n2;//取余
+//	//输出
+//	printf("-----计算器------");
+//	printf("\n %d+%d=%d", n1, n2, sum);
+//	printf("\n %d+%d=%d", n1, n2, sub);
+//	printf("\n %d+%d=%d", n1, n2, mul);
+//	printf("\n %d+%d=%d", n1, n2, div);
+//	printf("\n %d+%d=%d", n1, n2, mod);
+//	if (num % 2 == 0) {
+//		printf("\n %d是偶数", num);
+//	}
+//	else {
+//		printf("\n%d是奇数", num);
+//	}
+//}
+///*
+//-----计算器------
+// 10+5=15
+// 10+5=5
+// 10+5=50
+// 10+5=2
+// 10+5=0
+// 10是偶数
+//*/
