fcd6ae9a91ee61e8d3f32c8709384114bb78fe86
hero <1326172435@qq.com>
2022-04-21 08:44:26 +0800

    logic orperator

--- 6（logicOrperator逻辑运算符).cpp
+++ 6（logicOrperator逻辑运算符).cpp
@@ -1,30 +1,42 @@
-#include<stdio.h>
-void main() {
-
-	//逻辑与
-	//	double score = 70;
-	//	if (score >= 60 && score <= 80) {
-	//		printf("ok1");
-	//	}
-	//	else {
-	//		printf("not ok");
-	//	}
-	//}
-
-
-	//int a = 10, b = 99;
-	//if(a < 20 && b++>99) {//a=10 <20 为 1 ，b++先比较再自增，99并不大于99所以不执行第一个条件，而执行第二个条件
-	//	printf("ok");
-	//}
-	//else {
-	//	printf("\nnot ok ");
-	//}
-	//}
-
-	//逻辑或
-int a = 10, b = 99;
-if (a < 5 || b++>100) {//a<5
-	printf("ok");
-}
-	printf("b=%d\n", b);
-}
+//#include<stdio.h>
+//void main() {
+//
+//	//逻辑与
+//	//	double score = 70;
+//	//	if (score >= 60 && score <= 80) {
+//	//		printf("ok1");
+//	//	}
+//	//	else {
+//	//		printf("not ok");
+//	//	}
+//	//}
+//
+//
+//	//int a = 10, b = 99;
+//	//if(a < 20 && b++>99) {//a=10 <20 为 1 ，b++先比较再自增，99并不大于99所以不执行第一个条件，而执行第二个条件
+//	//	printf("ok");
+//	//}
+//	//else {
+//	//	printf("\nnot ok ");
+//	//}
+//	//}
+//
+//	//逻辑或
+////int a = 10, b = 99;
+////if (a < 5 || b++>100) {//a<5
+////	printf("ok");
+////}
+////	printf("b=%d\n", b);
+////}
+//
+//
+////逻辑非
+//	int score = 100;
+//	int res = score > 199;//score不大于199，为假，所以为假不执行
+//	if (res) {//为假不执行
+//		printf("ok");
+//	}
+//	if (!res) {//取反，为真，执行语句
+//		printf("not ok");
+//	}
+//}
